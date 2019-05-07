b0d99021605c06080bd4315f795f9580cf3bbdfc
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-05-07 15:19:54 -0600

    comment section updated

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -9,17 +9,65 @@ class CommentSection extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            comments: props.comments
+            comments: props.comments,
+            comment: '',
         };
     }
+
+    componentDidMount() {
+        const id = this.props.postId;
+        if (localStorage.getItem(id)) {
+            this.setState({
+                comments: JSON.parse(localStorage.getItem(this.postId))
+            });
+        } else {
+            this.setComments();
+        }
+    }
+
+    componentWillUnmount() {
+        this.setComments();
+    }
+
+    setComments = () => {
+        localStorage.setItem(
+            this.props.postId,
+            JSON.stringify(this.state.comments)
+        );
+    };
+
+    commentHandler = e => {
+        this.setState({
+            comment: e.target.value
+        });
+    };
+
+    handleCommentSubmit = e => {
+        e.preventDefault();
+        const newComment = {
+            text: this.state.comment,
+            username: 'username'
+        };
+        comments.push(newComment);
+        this.setState({
+            comments, comment: ''
+        });
+        setTimeout(() => {
+            this.setComments();
+        }, 500);
+    };
 
     render() {
         return (
             <div>
                 {this.state.comments.map((c, i) => <Comment key={i} comment={c} />)}
-                <CommentForm />
+                <CommentForm 
+                    comment={this.state.comment}
+                    submitComment={this.handleCommentSubmit}
+                    changeComment={this.commentHandler}
+                />
             </div>
-        )
+        );
     }
 }
 
