5d4b35133864a806eb0c1d534c9be880667dd82a
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-05-07 16:10:43 -0600

    post.js updated

--- instagram-app/src/components/PostContainer/Post.js
+++ instagram-app/src/components/PostContainer/Post.js
@@ -2,20 +2,54 @@ import React from 'react';
 import CommentSection from '../CommentSection/CommentSection';
 import PostHeader from './PostHeader';
 import './PostContainer.css';
+import Likes from './Likes';
 
-const Post = props => {
-    return (
-        <div className="post-container">
-            <PostHeader
-                username={props.post.username}
-                thumbnailUrl={props.post.thumbnailUrl}
-            />
-            <div className="post-img-container">
-                <img className="post-img" src={props.post.imageUrl} alt="post image" />
-            </div>
-            <CommentSection comments={props.post.comments} />
-        </div>
-    );
+class Post extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            likes: props.post.likes
+        };
+    }
+
+    incrementLike = () => {
+        let likes = this.state.likes + 1;
+        this.setState({likes});
+    };
+
+    render() {
+        return(
+            <div className="post-border">
+                <PostHeader
+                    username={this.props.post.username}
+                    thumbnailUrl={this.props.post.thumbnailUrl}
+                />
+                <div className="post-img-container">
+                    <img
+                        src={this.props.post.imageUrl}
+                        className="post-img"
+                        alt="post thumbnail" 
+                    />
+                </div>
+                <Likes
+                    incrementLike={this.incrementLike}
+                    likes={this.state.likes}
+                />
+                <CommentSection
+                    postId={this.props.post.imageUrl}
+                    comments={this.props.post.comments}
+                />
+                </div>
+        );
+    }
+}
+
+Post.propTypes = {
+    post: PropTypes.shape({
+        username: PropTypes.string,
+        thumbnailUrl: PropTypes.string,
+        imageUrl: Proptypes.string
+    })
 };
 
 export default Post;
