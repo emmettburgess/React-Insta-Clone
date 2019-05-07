0e28bad8eb1f6a7aa9fc3006c07f75a536cd5029
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-05-07 15:10:39 -0600

    logo added, comment form updated

--- instagram-app/src/components/CommentSection/CommentForm.js
+++ instagram-app/src/components/CommentSection/CommentForm.js
@@ -3,7 +3,9 @@ import React from 'react';
 const CommentForm = props => {
     return (
         <form>
-            <input type="text" placeholder="Comment..." />
+            <input type="text" placeholder="Comment..." 
+            onChange={props.changeComment}
+            />
         </form>
     );
 };
