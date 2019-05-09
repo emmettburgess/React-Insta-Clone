c390c0b965525bc98e9c9cf077b1c08ee27d12d7
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-05-09 16:22:06 -0600

    post css and searchbar.js updated

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,16 +1,81 @@
 import React from 'react';
-import './SearchBar.css';
+import styled from 'styled-components';
 
+const Header = styled.header`
+  border-bottom: 1px solid black;
+  height: 80px;
+  line-height: 70px;
+  display: flex;
+  justify-content: space-between;
+  background: white;
+`;
+
+const LogoHeader = styled.div`
+  width: 300px;
+  line-height: 75px;
+`;
+
+const LogoImage = styled.img`
+  height: 50%;
+  padding: 0px 15px;
+`;
+
+const Divider = styled.span`
+  width: 50px;
+`;
+
+const InputWrapper = styled.div`
+  width: 300px;
+  margin: auto;
+  display: flex;
+`;
+
+const SearchInput = styled.input`
+  height: 30px;
+  width: 300px;
+  text-align: center;
+`;
+
+const IconsWrapper = styled.div`
+  display: flex;
+  width: 300px;
+`;
+const SearchIcon = styled.i`
+  position: absolute;
+  display: inline;
+  top: 33px;
+  padding-left: 10px;
+`;
+const SocialDiv = styled.div`
+  width: 50px;
+`;
 const SearchBar = props => {
   return (
-    <div className="search-bar-wrapper">
-      <div className="image-wrapper">
-        <img alt="instagram logo" src="instagram-app\src\assets\IGlogo.png" className="logo-image" />
-      </div>
-      <div>
-        <input type="text" placeholder="Search" onKeyDown={props.searchPosts} />
-      </div>
-    </div>
+    <Header>
+      <LogoHeader>
+        <LogoImage alt="instagram camera" src="#" />
+        <Divider>|</Divider>
+        <LogoImage alt="instagram logo" src="instagram-app\src\assets\IGlogo.png" />
+      </LogoHeader>
+      <InputWrapper>
+        <SearchInput
+          type="text"
+          placeholder="Search"
+          onKeyDown={props.searchPosts}
+        />
+      </InputWrapper>
+      <IconsWrapper>
+        <SocialDiv>
+          <i className="far fa-compass" />
+        </SocialDiv>
+        <SocialDiv>
+          <i className="far fa-heart" />
+        </SocialDiv>
+        <SocialDiv>
+          <i className="far fa-user-circle" />
+        </SocialDiv>
+      </IconsWrapper>
+    </Header>
   );
 };
 
